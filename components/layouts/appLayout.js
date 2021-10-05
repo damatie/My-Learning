27b374c9c4b17b8fc56d5b40013323583738896a
SamuelPhases <obajisamuelekene@gmail.com>
2021-10-05 10:19:58 +0100

    still fixing

--- components/layouts/appLayout.js
+++ components/layouts/appLayout.js
@@ -1,28 +1,28 @@
-import {useState}  from 'react' 
-import ASideNav from '../navBar/ASideNav';
+// import {useState}  from 'react' 
+// import ASideNav from '../navBar/ASideNav';
 
-import HeadLayout from "./headLayout";
+// import HeadLayout from "./headLayout";
 
-function AppLayout({children}) {
+// function AppLayout({children}) {
 
     
-    const [reduceAppSideNav, setReduceAppSideNav] = useState(false)
+//     const [reduceAppSideNav, setReduceAppSideNav] = useState(false)
 
 
 
-    return (
-        <>
-            <HeadLayout title='Dashboard' />
-            <section className='bg-accent flex h-screen'>
-                <aside className={`${reduceAppSideNav ? 'bg-call-to-action md:w-3/12' :  'bg-call-to-action md:w-1/12'  }`}>
-                    <ASideNav reduceAppSideNav={reduceAppSideNav} setReduceAppSideNav={setReduceAppSideNav} />
-                </aside>
-                <main className='md:w-9/12'>
-                    {children}
-                </main>
-            </section>
-        </>
-    );
-}
+//     return (
+//         <>
+//             <HeadLayout title='Dashboard' />
+//             <section className='bg-accent flex h-screen'>
+//                 <aside className={`${reduceAppSideNav ? 'bg-call-to-action md:w-3/12' :  'bg-call-to-action md:w-1/12'  }`}>
+//                     <ASideNav reduceAppSideNav={reduceAppSideNav} setReduceAppSideNav={setReduceAppSideNav} />
+//                 </aside>
+//                 <main className='md:w-9/12'>
+//                     {children}
+//                 </main>
+//             </section>
+//         </>
+//     );
+// }
 
-export default AppLayout;
+// export default AppLayout;
