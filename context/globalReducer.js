9be4e1a5b03a09189cb8fddce1b6528b2d8dc0ae
SamuelPhases <obajisamuelekene@gmail.com>
2021-10-13 11:27:47 +0100

    edited page structure and removed unwanted files

---
+++ context/globalReducer.js
@@ -0,0 +1,15 @@
+export const initialState  =  {
+    user:'hello',
+    fromEditBox:true,
+    fromReplyBox:false,
+    replyForwardContainer:false,
+}
+
+const reducer = (state,action) => {
+    switch(action.type){
+            default:
+                return state;
+        }
+    }
+
+export default reducer
